154b3e08532f730c2228340f691cba2c858fca67
SHELL TOKYO <74878846+SHELLTOKYO@users.noreply.github.com>
2021-05-09 00:35:01 +0300

    Update Tokyo.sh

--- Tokyo.sh
+++ Tokyo.sh
@@ -8,10 +8,10 @@ clear
 printf "\033[91m#############<<TOKYO>>#############\n\r"
 printf "\033[92m#        This Tool Crate BY       #\n\r"
 printf "\033[92m#            SHELL TOKYO          #\n\r"
-printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m#        snapchat {ali.phone5}   #\n\r"
 printf "\033[91m#############<<TOKYO>>#############\n\r"
 sleep 3
-xdg-open https://t.me/Kurdish_Cracker0
+xdg-open https://t.me/S_pluss
 pkg update -y
 clear
 pkg upgrade
@@ -19,7 +19,7 @@ sleep 10
 clear
 printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[91m#   Chawre bka ta command dabzet  #\n\r"
-printf "\033[91m#      Snapchat {shell_tokyo}     #\n\r"
+printf "\033[91m#      Snapchat {ali.phone5}      #\n\r"
 printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
 sleep 5
 termux-setup-storage
@@ -27,103 +27,103 @@ clear
 sleep 3
 printf "\033[93m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[91m#  chawre bka ta python dabzet    #\n\r"
-printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[93m##########<<SHELL TOKYO>>##########\n\r"
 sleep 4
 pkg install python -y
 clear
 printf "\033[94m#############<<SHELL TOKYO>>############\n\r"
 printf "\033[92m#  pythonish ba sarkawtuy dawnload bw  #\n\r"
-printf "\033[92m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[92m#         Snapchat {ali.phone5}        #\n\r"
 printf "\033[94m#############<<SHELL TOKYO>>############\n\r"
 clear
 printf "\033[93m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[91m#  chawre bka ta python2 dabzet   #\n\r"
-printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[93m##########<<SHELL TOKYO>>##########\n\r"
 sleep 4
 pkg install python2 -y
 clear
 printf "\033[94m#############<<SHELL TOKYO>>############\n\r"
 printf "\033[92m#   pythonish ba sarkawtuy dawnload bw #\n\r"
-printf "\033[92m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[92m#         Snapchat {ali.phone5}        #\n\r"
 printf "\033[94m#############<<SHELL TOKYO>>############\n\r"
 clear
 printf "\033[91m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[95m#     chawre bka ta git dabzet    #\n\r"
-printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[91m##########<<SHELL TOKYO>>##########\n\r"
 sleep 4
 pkg install git -y
 clear
 printf "\033[94m#############<<SHELL TOKYO>>############\n\r"
 printf "\033[92m#    gitish ba sarkawtuy dawnload bw   #\n\r"
-printf "\033[92m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[92m#         Snapchat {ali.phone5}        #\n\r"
 printf "\033[94m#############<<SHELL TOKYO>>############\n\r"
 clear
 printf "\033[91m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[95m#     chawre bka ta pip2 dabzet   #\n\r"
-printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"  
+printf "\033[92m#        snapchat {ali.phone5}    #\n\r"  
 printf "\033[91m##########<<SHELL TOKYO>>##########\n\r"
 sleep 4
 pip2 install requests mechanize
 clear
 printf "\033[92m#############<<SHELL TOKYO>>############\n\r"
 printf "\033[95m#   pip2ish ba sarkawtuy dawnload bw   #\n\r"   
-printf "\033[95m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[95m#         Snapchat {ali.phone5}        #\n\r"
 printf "\033[92m#############<<SHELL TOKYO>>############\n\r"
 clear
 printf "\033[95m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[92m#     chawre bka ta nano dabzet   #\n\r"
-printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[95m##########<<SHELL TOKYO>>##########\n\r"
 sleep 4
 pkg install nano -y
 clear
 printf "\033[96m#############<<SHELL TOKYO>>############\n\r"
 printf "\033[91m#   nanoish ba sarkawtuy dawnload bw   #\n\r"
-printf "\033[95m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[95m#         Snapchat {ali.phone5}        #\n\r"
 printf "\033[92m#############<<SHELL TOKYO>>############\n\r"
 clear
 printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[93m#     chawre bka ta ruby dabzet   #\n\r"
-printf "\033[93m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[93m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
 sleep 4
 pkg install ruby-y
 clear
 printf "\033[97m#############<<SHELL TOKYO>>############\n\r"
 printf "\033[92m#   rubyish ba sarkawtuy dawnload bw   #\n\r"
-printf "\033[92m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[92m#         Snapchat {ali.phone5}        #\n\r"
 printf "\033[97m#############<<SHELL TOKYO>>############\n\r"
 clear
 printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[97m#      chawre bka ta php dabzet   #\n\r"
-printf "\033[97m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[97m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
 sleep 4
 pkg install php -y
 clear
 printf "\033[93m#############<<SHELL TOKYO>>############\n\r"
 printf "\033[92m#    phpish ba sarkawtuy dawnload bw   #\n\r"
-printf "\033[92m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[92m#         Snapchat {ali.phone5}        #\n\r"
 printf "\033[93m#############<<SHELL TOKYO>>############\n\r"
 clear
 printf "\033[91m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[94m#      chawre bka ta curl dabzet  #\n\r"
-printf "\033[94m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[94m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[91m##########<<SHELL TOKYO>>##########\n\r"
 sleep 4
 pkg install curl -y
 clear
 printf "\033[92m#############<<SHELL TOKYO>>############\n\r"
 printf "\033[95m#   curlish ba sarkawtuy dawnload bw   #\n\r"
-printf "\033[95m#         Snapchat {shell_tokyo}       #\n\r" 
+printf "\033[95m#         Snapchat {ali.phone5}        #\n\r" 
 printf "\033[92m#############<<SHELL TOKYO>>############\n\r"
 clear
 printf "\033[96m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[91m#     chawre bka ta figlet dabzet #\n\r"
-printf "\033[91m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[91m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[96m##########<<SHELL TOKYO>>##########\n\r"
 sleep 4
 pkg install figlet -y
@@ -131,14 +131,14 @@ clear
 
 printf "\033[96m#############<<SHELL TOKYO>>############\n\r"
 printf "\033[92m#   figletish ba sarkawtuy dawnload bw #\n\r"
-printf "\033[92m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[92m#         Snapchat {ali.phone5}        #\n\r"
 printf "\033[96m#############<<SHELL TOKYO>>############\n\r"
 clear
 figlet TOKYO
 
 printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[92m#       chawre bka ta zip dabzet  #\n\r"
-printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
 sleep 4
 pkg install zip -y
@@ -147,14 +147,14 @@ figlet TOKYO
 
 printf "\033[96m#############<<SHELL TOKYO>>############\n\r"
 printf "\033[92m#     zipish ba sarkawtuy dawnload bw  #\n\r"
-printf "\033[92m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[92m#         Snapchat {ali.phone5}        #\n\r"
 printf "\033[96m#############<<SHELL TOKYO>>############\n\r"
 clear
 figlet TOKYO
 
 printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[92m#     chawre bka ta unzip dabzet  #\n\r"
-printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
 sleep 4
 pkg install unzip -y 
@@ -163,14 +163,14 @@ figlet TOKYO
 
 printf "\033[91m#############<<SHELL TOKYO>>############\n\r"
 printf "\033[96m#   unzipish ba sarkawtuy dawnload bw  #\n\r"
-printf "\033[96m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[96m#         Snapchat {ali.phone5}        #\n\r"
 printf "\033[91m#############<<SHELL TOKYO>>############\n\r"
 clear
 figlet TOKYO
 
 printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[95m#      chawre bka ta fish dabzet  #\n\r"
-printf "\033[95m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[95m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
 skeep 4
 pkg install fish -y
@@ -179,14 +179,14 @@ figlet TOKYO
 
 printf "\033[91m#############<<SHELL TOKYO>>############\n\r"
 printf "\033[96m#   fishish ba sarkawtuy dawnload bw   #\n\r"
-printf "\033[96m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[96m#         Snapchat {ali.phone5}        #\n\r"
 printf "\033[91m#############<<SHELL TOKYO>>############\n\r"
 clear
 figlet TOKYO
 
 printf "\033[93m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[91m#   chawre bka ta openssl dabzet  #\n\r"
-printf "\033[91m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[91m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[93m##########<<SHELL TOKYO>>##########\n\r"
 sleep 4
 pkg install openssl-y
@@ -195,14 +195,14 @@ figlet TOKYO
 
 printf "\033[91m#############<<SHELL TOKYO>>############\n\r"
 printf "\033[96m#  opensslish ba sarkawtuy dawnload bw #\n\r"
-printf "\033[96m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[96m#         Snapchat {ali.phone5}        #\n\r"
 printf "\033[91m#############<<SHELL TOKYO>>############\n\r"
 clear
 figlet TOKYO
 
 printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[95m#    Tawaw dlm Joine Channle ba   #\n\r"
-printf "\033[95m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[95m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
 sleep 4
 clear
@@ -210,7 +210,7 @@ figlet TOKYO
 
 printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[92m#    Tawaw dlm Joine Channle ba   #\n\r"
-printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
 sleep 4
 clear
@@ -220,23 +220,23 @@ figlet TOKYO
 sleep 7
 printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[91m#        addm bka la snapchat     #\n\r"
-printf "\033[91m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[91m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
 clear
 figlet TOKYO
 printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
 printf "\033[91m#        addm bka la snapchat     #\n\r"
-printf "\033[91m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[91m#        snapchat {ali.phone5}   #\n\r"
 printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
 clear
 figlet snapchat
-xdg-open https://www.snapchat.com/add/shell_tokyo
+xdg-open https://www.snapchat.com/add/ali.phone5
 sleep 10
 clear
 printf "\033[91m#############<<TOKYO>>#############\n\r"
 printf "\033[92m#        This Tool Crate BY       #\n\r"
 printf "\033[92m#            SHELL TOKYO          #\n\r"
-printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m#        snapchat {ali.phone5}    #\n\r"
 printf "\033[91m#############<<TOKYO>>#############\n\r"
 sleep 4
 figlet Bay
